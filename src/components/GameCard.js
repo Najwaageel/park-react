e90a8613dcc1781d86da52630601e84c5b0360bd
Najwaageel <Najwa.Ageel.abt.js@tuwaiq.edu.sa>
2022-01-18 12:20:02 +0300

    v

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -16,9 +16,9 @@ function GameCard(props) {
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
-          // height: 400,
+          height: 400,
           // width:255,
-          height: 500,
+          // height: 500,
           width:300,
           marginBottom:30,
         }}
